4550eca48288e8031755f08baf5cd82a60ed559f
Julien <peloton@lal.in2p3.fr>
2021-08-16 08:56:17 +0200

    Bump version to 2.5 (#100)

--- fink_client/__init__.py
+++ fink_client/__init__.py
@@ -12,5 +12,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "2.4"
+__version__ = "2.5"
 __schema_version__ = "distribution_schema_fink_ztf_{}.avsc"
