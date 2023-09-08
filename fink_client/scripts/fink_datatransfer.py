c5a9ddb722b82af97bbf7b22bc1a7b8a0682c357
JulienPeloton <peloton@lal.in2p3.fr>
2023-09-08 11:32:47 +0200

    Restore the license header

--- fink_client/scripts/fink_datatransfer.py
+++ fink_client/scripts/fink_datatransfer.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# Copyright 2023 AstroLab Software
+# Author: Julien Peloton, Saikou Oumar BAH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """ Kafka consumer to listen and archive Fink streams
     from the data transfer service
 """
